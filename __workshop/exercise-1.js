4d1fea653ddbfbc1691051344d9ff5dc22b22cb7
Stephanos Criticos <scritico@unb.ca>
2020-07-10 16:19:09 -0400

    exercise 1- 2.3

--- __workshop/exercise-1.js
+++ __workshop/exercise-1.js
@@ -1,17 +1,31 @@
 // Exercise 1
 // ------------
-const arrayOfWords = ['cucumber', 'tomatos', 'avocado'];
-const complicatedArray = ['cucumber', 44, true];
+const arrayOfWords = ["cucumber", "tomatos", "avocado"];
+const complicatedArray = ["cucumber", 44, true];
 
 const makeAllCaps = (array) => {
-  // write some code
+  myPromise = new Promise((resolve, reject) => {
+    const check = array.every((elements) => {
+      return typeof elements === "string";
+    });
+    if (check === true) {
+      const upperCaseArray = array.map((element) => {
+        return element.toUpperCase();
+      });
+      resolve(upperCaseArray);
+    } else {
+      reject("error");
+    }
+  });
+  return myPromise;
 };
-
 const sortWords = (array) => {
-  // write some code
+  const sorting = array.sort();
+  return sorting;
 };
 
 // Calling (testing)
+
 makeAllCaps(arrayOfWords)
   .then(sortWords)
   .then((result) => console.log(result))
